ef76ee19019dc6996a32762d3f7c738ea5a5f050
Zachary Anderson <zacharyanderson0423@gmail.com>
2025-09-03 23:10:12 -0700

    Pushing M6 Assignment 9-3-25

---
+++ module-6/FanTest.java
@@ -0,0 +1,22 @@
+public class FanTest {
+    public static void main(String[] args) {
+        // Fan using default constructor
+        Fan fan1 = new Fan();
+
+        // Fan using argument constructor
+        Fan fan2 = new Fan(Fan.FAST, true, 10, "blue");
+
+        // Print initial states
+        System.out.println("Fan 1: " + fan1);
+        System.out.println("Fan 2: " + fan2);
+
+        // Update Fan 1
+        fan1.setOn(true);
+        fan1.setSpeed(Fan.MEDIUM);
+        fan1.setRadius(8);
+        fan1.setColor("green");
+
+        // Print updated state
+        System.out.println("Updated Fan 1: " + fan1);
+    }
+}
